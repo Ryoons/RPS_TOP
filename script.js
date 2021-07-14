db32158a1672e78582962c49353ad53cdf2bec8e
Ryoons <ryanz35@hotmail.com>
2021-07-14 15:22:49 -0400

    added game loop

--- script.js
+++ script.js
@@ -1,46 +1,50 @@
 
-// Function that generates random choice as the computer
+function game() {
+    function computerPlay() {
+        let computerChoice = ['Rock', 'Paper', 'Scissors'];
+        let randomChoice = Math.floor(Math.random() * computerChoice.length)
+        return computerChoice[randomChoice]
+    }
+    computerPlay();
 
-function computerPlay() {
-    let computerChoice = ['Rock', 'Paper', 'Scissors'];
-    let randomChoice = Math.floor(Math.random() * computerChoice.length)
-    return computerChoice[randomChoice]
+    let computerSelection = computerPlay();
+    const playerSelection = computerPlay();
+
+    console.log('Player:' + playerSelection)
+    console.log('Computer:' + computerSelection)
+
+    function rpsRound(playerSelection, computerSelection) {
+
+        if (playerSelection === "Rock") {
+            if (computerSelection === "Scissors") {
+                console.log("You Win! Rock beats Scissors")
+            } if (computerSelection === "Rock") {
+                console.log("Tie! Rock ties Rock")
+            } else {
+                console.log("You Lose! Paper beats Rock")
+            }
+        }
+        if (playerSelection === "Paper") {
+            if (computerSelection === "Rock") {
+                console.log("You Win! Paper beats Rock")
+            } if (computerSelection === "Paper") {
+                console.log("Tie! Paper ties Paper")
+            } else {
+                console.log("You Lose! Scissors beats Paper")
+            }
+        }
+        if (playerSelection === "Scissors") {
+            if (computerSelection === "Paper") {
+                console.log("You Win! Scissors beats Paper")
+            } if (computerSelection === "Scissors") {
+                console.log("Tie! Scissors ties Scissors")
+            } else {
+                console.log("You Lose! Scissors beats Paper")
+            }
+        }
+
+    }
+    rpsRound(playerSelection, computerSelection);
+
 }
-computerPlay();
-let computerSelection = computerPlay();
-const playerSelection = "Rock";
-
-console.log('Player:' + playerSelection)
-console.log('Computer:' + computerSelection)
-
-function rpsRound(playerSelection, computerSelection) {
-    
-    if (playerSelection === "Rock") {
-        if (computerSelection === "Scissors") {
-            return "You Win! Rock beats Scissors"
-        } if (computerSelection === "Rock") {
-            return "Tie! Rock ties Rock"
-        } else {
-            return "You Lose! Paper beats Rock"
-        }
-    }
-    if (playerSelection === "Paper") {
-        if (computerSelection === "Rock") {
-            return "You Win! Paper beats Rock"
-        } if (computerSelection === "Paper") {
-            return "Tie! Paper ties Paper"
-        } else {
-            return "You Lose! Scissors beats Paper"
-        }
-    }
-    if (playerSelection === "Scissors") {
-        if (computerSelection === "Paper") {
-            return "You Win! Scissors beats Paper"
-        } if (computerSelection === "Scissors") {
-            return "Tie! Scissors ties Scissors"
-        } else {
-            return "You Lose! Scissors beats Paper"
-        }
-    }
-    
-}
+game();

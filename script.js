ad3bf9b6bd9d20f6f378c2a4b9d03e287f4eaa45
Ryoons <ryanz35@hotmail.com>
2021-07-14 14:28:43 -0400

    added computerPlay function getRandomInt and rpsRound functions

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 
-
 function computerPlay() {
     let computerChoice = ['Rock', 'Paper', 'Scissors'];
     return computerChoice[getRandomInt(3)]
 }
 function getRandomInt(max) {
     return Math.floor(Math.random() * max)
+}
+
+const computerSelection = computerPlay();
+const playerSelection = "Rock";
+
+function rpsRound(playerSelection, computerSelection) {
+    
+    if (playerSelection = "Rock") {
+        if (computerSelection = "Scissors") {
+            return "You Win! Rock beats Scissors"
+        } if (computerSelection = "Rock") {
+            return "Tie! Rock ties Rock"
+        }
+    }
 }
